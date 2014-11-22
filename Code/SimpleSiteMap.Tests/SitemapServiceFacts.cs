70dccb222eff2b977c9d0fc88e2d660926b9a29a
Pure Krome <github@world-domination.com.au>
2014-11-22 22:53:01 +1100

    Better error checks so AppVeyor won't cry.

--- Code/SimpleSiteMap.Tests/SitemapServiceFacts.cs
+++ Code/SimpleSiteMap.Tests/SitemapServiceFacts.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 using Shouldly;
 using SimpleSitemap.Core;
 using Xunit;
@@ -27,7 +29,7 @@ namespace SimpleSiteMap.Tests
             }
 
             [Fact]
-            public void GivenSomDataThatIsLargerThanASinglePage_ConvertToSiteMapFacts_CreatesASiteMapWithNoNodes()
+            public void GivenSomDataThatIsLargerThanASinglePage_ConvertToSiteMapFacts_CreatesASiteMapResult()
             {
                 // Arrange.
                 var data = CreateFakeSitemapNodes(100, new DateTime(2014, 11, 21, 18, 58, 00));
@@ -37,14 +39,13 @@ namespace SimpleSiteMap.Tests
                 var result = siteMapService.ConvertToXmlSiteMap(data, 10);
 
                 // Assert.
-                var resultXml = File.ReadAllText("Result Data\\100ItemsWithPageSize10.xml");
+                var expectedXml = File.ReadAllText("Result Data\\100ItemsWithPageSize10.xml");
 
-                // My sample data is a bit messed up - which is why I have to do the `replace`.
-                result.Replace("\r", string.Empty).ShouldBe(resultXml);
+                CompareTwoSitemapDocuments(result, expectedXml);
             }
 
             [Fact]
-            public void GivenSomeDataThatIsTheSameSizeAsASinglePage_ConvertToSiteMapFacts_CreatesASiteMapWithNoNodes()
+            public void GivenSomeDataThatIsTheSameSizeAsASinglePage_ConvertToSiteMapFacts_CreatesAUrlSetResult()
             {
                 // Arrange.
                 var data = CreateFakeSitemapNodes(2, new DateTime(2014, 11, 21, 18, 58, 00));
@@ -54,14 +55,14 @@ namespace SimpleSiteMap.Tests
                 var result = siteMapService.ConvertToXmlSiteMap(data, 2);
 
                 // Assert.
-                var resultXml = File.ReadAllText("Result Data\\2ItemsWithPageSize2.xml");
+                var expectedXml = File.ReadAllText("Result Data\\2ItemsWithPageSize2.xml");
 
                 // My sample data is a bit messed up - which is why I have to do the `replace`.
-                result.Replace("\r", string.Empty).ShouldBe(resultXml);
+                CompareTwoUrlsetDocuments(result, expectedXml);
             }
 
             [Fact]
-            public void GivenNoData_ConvertToSiteMapFacts_CreatesASiteMapWithNoNodes()
+            public void GivenNoData_ConvertToSiteMapFacts_CreatesAUrlsetWithNoNodes()
             {
                 // Arrange.
                 var data = CreateFakeSitemapNodes(0, new DateTime(2014, 11, 21, 18, 58, 00));
@@ -71,10 +72,72 @@ namespace SimpleSiteMap.Tests
                 var result = siteMapService.ConvertToXmlSiteMap(data, 10);
 
                 // Assert.
-                var resultXml = File.ReadAllText("Result Data\\0ItemsWithPageSize10.xml");
+                var expectedXml = File.ReadAllText("Result Data\\0ItemsWithPageSize10.xml");
 
                 // My sample data is a bit messed up - which is why I have to do the `replace`.
-                result.Replace("\r", string.Empty).ShouldBe(resultXml);
+                CompareTwoUrlsetDocuments(result, expectedXml);
+            }
+
+            private static void CompareTwoSitemapDocuments(string actualXml, string expectedXml)
+            {
+                actualXml.ShouldNotBeNullOrEmpty();
+                expectedXml.ShouldNotBeNullOrEmpty();
+
+                var actualXmlDocument = XDocument.Parse(actualXml);
+                var expectedXmlDocument = XDocument.Parse(expectedXml);
+
+                actualXmlDocument.ToString().ShouldStartWith("<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">");
+
+                expectedXmlDocument.DescendantNodes().Count().ShouldBe(actualXmlDocument.DescendantNodes().Count());
+
+                var actuaElements = actualXmlDocument.Root.Elements().ToList();
+                var expectedElements = expectedXmlDocument.Root.Elements().ToList();
+
+                for (int i = 0; i < actuaElements.Count ; i++)
+                {
+                    var actualChildrenElements = actuaElements[i].Elements().ToList();
+                    var expectedChildrenElements = expectedElements[i].Elements().ToList();
+
+                    // loc.
+                    actualChildrenElements[0].Value.ShouldBe(expectedChildrenElements[0].Value);
+
+                    // lastmod.
+                    actualChildrenElements[1].Value.ShouldBe(expectedChildrenElements[1].Value);
+                }
+            }
+
+            private static void CompareTwoUrlsetDocuments(string actualXml, string expectedXml)
+            {
+                actualXml.ShouldNotBeNullOrEmpty();
+                expectedXml.ShouldNotBeNullOrEmpty();
+
+                var actualXmlDocument = XDocument.Parse(actualXml);
+                var expectedXmlDocument = XDocument.Parse(expectedXml);
+
+                actualXmlDocument.ToString().ShouldStartWith("<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"");
+
+                expectedXmlDocument.DescendantNodes().Count().ShouldBe(actualXmlDocument.DescendantNodes().Count());
+
+                var actuaElements = actualXmlDocument.Root.Elements().ToList();
+                var expectedElements = expectedXmlDocument.Root.Elements().ToList();
+
+                for (int i = 0; i < actuaElements.Count; i++)
+                {
+                    var actualChildrenElements = actuaElements[i].Elements().ToList();
+                    var expectedChildrenElements = expectedElements[i].Elements().ToList();
+
+                    // loc.
+                    actualChildrenElements[0].Value.ShouldBe(expectedChildrenElements[0].Value);
+
+                    // lastmod.
+                    actualChildrenElements[1].Value.ShouldBe(expectedChildrenElements[1].Value);
+
+                    // changefreq.
+                    actualChildrenElements[2].Value.ShouldBe(expectedChildrenElements[2].Value);
+
+                    // priority.
+                    actualChildrenElements[3].Value.ShouldBe(expectedChildrenElements[3].Value);
+                }
             }
         }
     }
